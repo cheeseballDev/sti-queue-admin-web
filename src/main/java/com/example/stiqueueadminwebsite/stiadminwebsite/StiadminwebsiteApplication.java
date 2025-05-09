1ef344cecae004c82506e08d72f5c97c6e13c042
Matthew <cheeseballbusinessemail@gmail.com>
2025-05-09 16:59:47 +0800

    First commit

---
+++ src/main/java/com/example/stiqueueadminwebsite/stiadminwebsite/StiadminwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.stiqueueadminwebsite.stiadminwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StiadminwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StiadminwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stiqueueadminwebsite/stiadminwebsite/StiadminwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stiqueueadminwebsite.stiadminwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StiadminwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
